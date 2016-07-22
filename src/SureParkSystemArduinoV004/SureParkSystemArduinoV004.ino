ba4440ab6902b205cc86b11b3e510b16b7905ac6
Byungwook Lee <Byungwook Lee>
2016-07-22 17:53:36 -0400

    Add comment of arduino program for readability

--- src/SureParkSystemArduinoV004/SureParkSystemArduinoV004.ino
+++ src/SureParkSystemArduinoV004/SureParkSystemArduinoV004.ino
@@ -1,3 +1,46 @@
+/****************************************************************
+ * File: SureParkSystemArduinoV2.0
+ * Project: Ajou University Summer CMU Software Engineering Program
+ * Copyright: Copyright (c) 2016 DH1002 - "Cheony"
+ * versions:
+ * 1.0 July 20, 2016 - Initial version
+ * 2.0 July 22, 2016 - Interim version
+ * 
+ * Description:
+ * 
+ * This is a part of Sure-Park System. This program reads the sensor
+ * calues and send them to Local server. It also receives the command
+ * data from Local server and executes specific tasks (open the entry
+ * gate and exit gate, and turn on the LED of exit or entry gate and 
+ * parking space.
+ * 
+ * Parameters: None
+ * 
+ * Internal Methods:
+ * 
+ * void sendToServer() - Identify EntryBeamState and ExitBeamState 
+ * from IR sensors which are EntryBeamRcvr and ExitBeamRcvr.
+ * Identify Stall1SensorVal, 2, 3, and 4 from illumination sensors
+ * which are Stall1SensorPin, 2, 3, and 4. It calls recvFromServer()
+ * function.
+ * 
+ * void recvFromServer() - Receive the command date from Local sercer.
+ * It works a lot of tasks which are opening the gates, and turning 
+ * on the LEDs.
+ * 
+ * void ProximityVal(int sensorIn) - returns an integer value from
+ * the QTI sensor that is an indication of the amount of light be 
+ * reflected into the sensor.
+ *
+ * void InitEntryExitLEDs() - Initializes the entry and exit LEDs
+ * to ensure they are off.
+ * 
+ * void printConnectionStatus() - Show the status of connection
+ * 
+ * void turnOffParkingLEDs() - It turns off all of the LEDs of 
+ * the parking spaces.
+ ****************************************************************/
+
 #include <Servo.h> 
 #include <Thread.h>
 #include <ThreadController.h>
@@ -317,7 +360,23 @@ void loop(){
   sendToServer();
 }
 
-/* ParkingStallSensor read */
+/*********************************************************************
+* long ProximityVal(int Pin)
+* Parameters:            
+* int pin - the pin on the Arduino where the QTI sensor is connected.
+*
+* Description:
+* QTI schematics and specs: http://www.parallax.com/product/555-27401
+* This method initalizes the QTI sensor pin as output and charges the
+* capacitor on the QTI. The QTI emits IR light which is reflected off 
+* of any surface in front of the sensor. The amount of IR light 
+* reflected back is detected by the IR resistor on the QTI. This is 
+* the resistor that the capacitor discharges through. The amount of 
+* time it takes to discharge determines how much light, and therefore 
+* the lightness or darkness of the material in front of the QTI sensor.
+* Given the closeness of the object in this application you will get
+* 0 if the sensor is covered
+***********************************************************************/
 long ProximityVal(int Pin)
 {
   long duration = 0;
